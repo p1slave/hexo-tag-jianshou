f2b335593f0e0cc8e95369b4eb2c5d1ca04558b3
p1slave <p1slave@protonmail.com>
2022-02-07 21:03:21 -0600

    Add media tag for jianshou platform

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # hexo-tag-jianshou
+An useful media tag for digital assets sold on [https://jianshou.online](https://tjianshou.online) to be embeded in [Hexo](https://hexo.io) blog.
+
+## Installation
+Run the following command in the root directory of hexo:
+```
+npm install hexo-tag-jianshou --save
+```
+
+### Usage ###
+Wrap the hash Id with `{% jianshou %}` tag and put it somewhere in the text. 
+Note that the preview picture will not be shown if we embed it in the webpage and the preview picture is only available in the direct link of the asset.
+```
+{% jianshou asset_hash_id %}
+```
+
+## License ##
+
+MIT
---
+++ index.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var pathFn = require('path');
+const { htmlTag, url_for } = require('hexo-util');
+
+const rUrl = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@.\w_]*)#?(?:[.!/\\w]*))?)/;
+const rMeta = /["']?([^"']+)?["']?\s*["']?([^"']+)?["']?/;
+const rImgFile = /([^\s]+(\.(jpg|JPG|png|PNG|gif|GIF|bmp|BMP))$)/;
+
+/**
+* pcloud Image tag
+*
+* Syntax:
+*   {% pcloudimg [class names] imageFile [width] [height] [title text [alt text]] %}
+*/
+
+function jianshouTag(args, content) {
+	// link = "<div class="radish-content" data-hashid="xxxxxx"></div><script src="https://jianshou.online/js/embed.js"></script>"
+    const item = args.shift();
+	const div = htmlTag('div', {
+		"class": "radish-content",
+		"data-hashid": item
+	});
+
+	const img = htmlTag('img', {
+		"class": "",
+		"src": "https://jgloinc.com/wp-content/uploads/2021/02/How-to-Make-an-Image-a-Clickable-Link.jpg"
+	})
+
+	const script = htmlTag('script', {
+		"src": "https://jianshou.online/js/embed.js"
+	})
+
+    return div + script;
+};
+
+hexo.extend.tag.register("jianshou", jianshouTag, {async: false, ends: false});
