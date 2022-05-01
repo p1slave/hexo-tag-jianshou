b935f9c31e22e6e8fdfb19bd15370bd0abad7456
p1slave <p1slave@protonmail.com>
2022-05-01 02:03:51 -0500

    Make the script tag in generated html to be optional and replaceable

--- README.md
+++ README.md
@@ -10,9 +10,12 @@ npm install hexo-tag-jianshou --save
 ## Usage
 Wrap the hash Id with `{% jianshou %}` tag and put it somewhere in the text. 
 Note that the preview picture will not be shown if we embed it in the webpage and the preview picture is only available in the direct link of the asset.
+
 ```
-{% jianshou asset_hash_id %}
+{% jianshou hashid [nojs | javascript url] %}
 ```
+
+The `hashid` can be followed by one additional argument. If the argument is "nojs", the script tag will not be included otherwise the default JS path [https://jianshou.online/js/embed.js](https://jianshou.online/js/embed.js) will be used to render the link of selling items. You can also specify your own url path ending in `.js` as an argument to replace the default JS file. Ideally, you may want to host that `embed.js` file under your own domain because the owner of the jianshou does not want to deploy it to CDN and it is quite slow to download this file from abroad. 
 
 ## License ##
 MIT
--- index.js
+++ index.js
@@ -1,30 +1,43 @@
 'use strict';
 
-var pathFn = require('path');
-const { htmlTag, url_for } = require('hexo-util');
+const { htmlTag } = require('hexo-util');
 
 const rUrl = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@.\w_]*)#?(?:[.!/\\w]*))?)/;
 const rMeta = /["']?([^"']+)?["']?\s*["']?([^"']+)?["']?/;
 const rImgFile = /([^\s]+(\.(jpg|JPG|png|PNG|gif|GIF|bmp|BMP))$)/;
 
 /**
-* pcloud Image tag
+* Jianshou selling item html tag
 *
 * Syntax:
-*   {% pcloudimg [class names] imageFile [width] [height] [title text [alt text]] %}
+*   {% jianshou hashid ["nojs" | Javascript url] %}
 */
 
 function jianshouTag(args, content) {
-	// link = "<div class="radish-content" data-hashid="xxxxxx"></div><script src="https://jianshou.online/js/embed.js"></script>"
-    const item = args.shift();
+	// Below is the example of generated html for jianshou tag.
+	// "<div class="radish-content" data-hashid="xxxxxx"></div><script src="https://jianshou.online/js/embed.js"></script>"
+    const hashid = args.shift();
 	const div = htmlTag('div', {
 		"class": "radish-content",
-		"data-hashid": item
+		"data-hashid": hashid
 	}, '');
 
-	const script = htmlTag('script', {
+	let script = htmlTag('script', {
 		"src": "https://jianshou.online/js/embed.js"
 	}, '');
+
+	if (args.length > 0) {
+		const item = args.shift();
+		if(item == 'nojs') {
+			script = '';
+		} else if (/^(http|https):\/\/\S+.js$/.test(item)) {
+			// Embed your own jianshou embed.js script
+			script = htmlTag('script', {
+				"src": item
+			}, '');
+
+		}
+	}
 
     return div + script;
 };
